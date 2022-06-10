25732c1842bbb589fe6f6e629db61ca6425437df
Thinh922001 <lecuongthinh2001@gmal.com>
2022-06-10 11:06:20 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    var a=5;
+    var b=6;
+    var c=a+b;
+  res.send('<strong>Hello Word</strong>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
